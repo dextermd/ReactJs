738dd3115223b9b2bb18383498efa791b99ecf09
dextermd <dextermdpay@gmail.com>
2024-02-03 13:03:03 +0200

    useEffect(() => {

--- ls-project/src/components/navbar/Navbar.jsx
+++ ls-project/src/components/navbar/Navbar.jsx
@@ -1,10 +1,7 @@
-// Lib
-import React from 'react'
-// Style
-import './Navbar.scss'
-// Img
-import Logo from '../../assets/img/logo.png'
-import SpiderMan from '../../assets/icons/spider2.svg'
+import React from 'react';
+import './Navbar.scss';
+import Logo from '../../assets/img/logo.png';
+import SpiderMan from '../../assets/icons/spider2.svg';
 import { CiShoppingCart as CartIcon } from "react-icons/ci";
 import { Link } from 'react-router-dom';
 
@@ -13,23 +10,23 @@ const Navbar = () => {
     <div className="container">
       <nav className='d-flex justify-between align-center pt-40'>
         <div>
-        <Link to={"/"}>
-          <a href="#"><img src={Logo} alt="logo" /></a>
-        </Link>
+          <Link to={"/"}>
+            <img src={Logo} alt="logo" />
+          </Link>
         </div>
         <div className="links d-flex justify-between align-center">
           <a className='nav_link' href="">About</a>
           <a className='nav_link' href="">Contacts</a>
           <a className='nav_link' href="">News</a>
-          <a className='nav_link' href="">Parteners</a>
+          <a className='nav_link' href="">Partners</a>
         </div>
         <div className="d-flex justify-between align-center links">
           <a>
-              <CartIcon size={32}/>
-              <span className="ml-15">00.00 USD</span>
+            <CartIcon size={32}/>
+            <span className="ml-15">00.00 USD</span>
           </a>
           <a>
-              <img className='spiderman_icon' src={SpiderMan} alt="spiderman icon" />
+            <img className='spiderman_icon' src={SpiderMan} alt="spiderman icon" />
           </a>
         </div>
       </nav>
--- ls-project/src/components/shop_card/ShopCard.jsx
+++ ls-project/src/components/shop_card/ShopCard.jsx
@@ -5,33 +5,37 @@ import './ShopCard.scss'
 import Add from '../../assets/icons/add.svg'
 import Ok from '../../assets/icons/ok.svg'
 
-import comicsData from '../../../comics.json';
 
-const ShopCard = () => {
+const ShopCard = ({ items }) => {
+
     const [icon = Add, setIsAdded] = useState();
     const change = () => {
         icon == Add ? setIsAdded(Ok) : setIsAdded(Add);
     } 
 
-    const comicsList = comicsData.map((comic, idx) => (
-        <div key={comic.id} className="card">
-            <img src={comic.imgUrl} alt={comic.name} />
-            <h5>{comic.name}</h5>
-            <div className='d-flex align-center justify-between'>
-                <div className='d-flex flex-col justify-between align-center bloc_price'>
-                    <span>Price: </span>
-                    <b>{comic.price} USD</b>
-                </div>
-                <a href="#" onClick={change(idx)} className='btn'>
-                    <img src={icon} alt="" srcset="" />
-                </a>
-            </div>
-        </div>
-    ));
-
   return (
         <div className="d-flex pt-50 card_container">
-            {comicsList}
+            {
+                items.map((item) => {
+                    
+                    return (
+                        <div key={item.id} className="card">
+                            <img src={item.imgUrl} alt=""/>
+                            <h5>{item.name}</h5>
+                            <div className='d-flex align-center justify-between'>
+                                <div className='d-flex flex-col justify-between align-center bloc_price'>
+                                    <span>Price: {item.price}</span>
+                                    <b>USD</b>
+                                </div>
+                                <a onClick={change} className='btn'>
+                                    <img src={icon} alt="" srcSet="" /> 
+                                </a>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+
         </div>
   )
 }
